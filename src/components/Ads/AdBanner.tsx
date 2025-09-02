12cc696032f177d0dc918700612b3c1ab565ca7d
Zayn365 <zenblockface@gmail.com>
2025-09-03 03:38:15 +0500

    feat: add AdBanner component and integrate it into RightSidebar

---
+++ src/components/Ads/AdBanner.tsx
@@ -0,0 +1,30 @@
+// components/AdBanner.tsx
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function AdBanner() {
+  const pushedRef = useRef(false);
+
+  useEffect(() => {
+    if (pushedRef.current) return;
+    try {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+      pushedRef.current = true;
+    } catch (_) {}
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-3742696288532239"
+      data-ad-slot=""
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+      data-adtest="off"
+    />
+  );
+}
--- src/components/layout/sidebar/right/index.tsx
+++ src/components/layout/sidebar/right/index.tsx
@@ -4,6 +4,8 @@ import { Search } from "lucide-react";
 import React from "react";
 import Market from "./market";
 import TopTokens from "./top-tokens";
+import AdBanner from "@/components/Ads/AdBanner";
+import Script from "next/script";
 
 export default function RightSidebar() {
   return (
@@ -11,7 +13,18 @@ export default function RightSidebar() {
       side="right"
       className="hidden max-w-[280px] justify-start border-l lg:flex dark:bg-[#1d1c34] bg-[#FFFFFF] h-screen overflow-y-auto slim-scrollbar sticky top-0"
     >
-      <SidebarContent className="dark:bg-[#1d1c34] bg-[#FFFFFF]">
+      <p className="text-[12px] opacity-[0.7] mt-1 ml-1">Advertisement</p>
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3742696288532239"
+        crossOrigin="anonymous"
+      />
+      <AdBanner />
+      <Script id="adsbygoogle-push" strategy="afterInteractive">
+        {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+      </Script>
+      {/* <SidebarContent className="dark:bg-[#1d1c34] bg-[#FFFFFF]">
         <div>
           <div className="p-4">
             <InputWithIcons
@@ -22,7 +35,7 @@ export default function RightSidebar() {
           <Market />
           <TopTokens />
         </div>
-      </SidebarContent>
+      </SidebarContent> */}
     </Sidebar>
   );
 }
